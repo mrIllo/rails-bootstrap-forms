900b06d88b876776bc27b3301f77c723465b1272
Christoph Illnar <illnar@communitor.com>
2019-08-26 14:44:33 +0200

    switched from :prompt to :include_blank for placeholder of collection_select, to have it always included

--- lib/bootstrap_form/inputs/collection_select.rb
+++ lib/bootstrap_form/inputs/collection_select.rb
@@ -12,9 +12,11 @@ module BootstrapForm
         def collection_select_with_bootstrap(method, collection, value_method, text_method, options={}, html_options={})
           form_group_builder(method, options, html_options) do
             html_options = translated_options_from_i18n_scope(method, options, html_options)
-            # special use case for promt/placeholders in collection_select
+            # special use case for prompt/placeholders in collection_select
+            # prompt will only be included if method returns nil value
+            # include_blank will always include a blank value
             if (placeholder = html_options.delete(:placeholder)).present?
-              options[:prompt] ||= placeholder
+              options[:include_blank] ||= placeholder
             end
             input_with_error(method) do
               collection_select_without_bootstrap(method, collection, value_method, text_method, options, html_options)
