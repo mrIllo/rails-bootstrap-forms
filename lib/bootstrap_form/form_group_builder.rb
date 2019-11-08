9b9571bb346f69d784f8ca08e7e5a8c2014157a3
Christoph Illnar <illnar@communitor.com>
2019-11-08 01:03:22 +0100

    Added new option :label_scope to form_group_builder to help translating form group labels

--- lib/bootstrap_form/form_group_builder.rb
+++ lib/bootstrap_form/form_group_builder.rb
@@ -10,10 +10,10 @@ module BootstrapForm
       no_wrapper = options[:wrapper] == false
 
       options = form_group_builder_options(options, method)
-      form_group_options = form_group_opts(options, form_group_css_options(method, html_options.try(:symbolize_keys!), options))
+      form_group_options = form_group_opts(options, method, form_group_css_options(method, html_options.try(:symbolize_keys!), options))
       options.except!(
         :help, :icon, :label_col, :control_col, :add_control_col_class, :layout, :skip_label, :label, :label_class,
-        :hide_label, :skip_required, :label_as_placeholder, :wrapper_class, :wrapper
+        :hide_label, :skip_required, :label_as_placeholder, :wrapper_class, :wrapper, :label_scope
       )
       if no_wrapper
         yield
@@ -39,7 +39,7 @@ module BootstrapForm
       options
     end
 
-    def form_group_opts(options, css_options)
+    def form_group_opts(options, method, css_options)
       wrapper_options = options[:wrapper]
       form_group_options = {
         id: options[:id], help: options[:help], icon: options[:icon],
@@ -49,22 +49,23 @@ module BootstrapForm
       }
 
       form_group_options.merge!(wrapper_options) if wrapper_options.is_a?(Hash)
-      form_group_options[:label] = form_group_label(options, css_options) unless options[:skip_label]
+      form_group_options[:label] = form_group_label(options, method, css_options) unless options[:skip_label]
       form_group_options
     end
 
-    def form_group_label(options, css_options)
+    def form_group_label(options, method, css_options)
       hash = {
-        text: form_group_label_text(options[:label]),
+        text: form_group_label_text(options[:label], method, options[:label_scope]),
         class: form_group_label_class(options),
         required: options[:required]
       }.merge(css_options[:id].present? ? { for: css_options[:id] } : {})
       hash
     end
 
-    def form_group_label_text(label)
+    def form_group_label_text(label, method, label_scope)
       text = label[:text] if label.is_a?(Hash)
       text ||= label if label.is_a?(String)
+      text ||= I18n.translate(method, scope: label_scope) if label_scope.present?
       text
     end
 
